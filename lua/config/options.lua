b1c55de681116295f9a610a6521f6dc19c7cc288
pysnow530 <pysnow530@163.com>
2024-01-12 14:43:40 +0800

    Add neovide options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,4 +2,11 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.spelllang = {}
+
+-- neovide config
+vim.g.neovide_input_macos_alt_is_meta = true
 vim.g.neovide_cursor_vfx_mode = "pixiedust"
+vim.g.neovide_transparency = 0.98
+vim.g.neovide_floating_blur_amount_x = 2.0
+vim.g.neovide_floating_blur_amount_y = 2.0
+vim.g.neovide_hide_mouse_when_typing = true
