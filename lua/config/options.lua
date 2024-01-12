db39bd9e7282973162f2b5c5f1b1967b2df4dd15
pysnow530 <pysnow530@163.com>
2024-01-12 12:26:20 +0800

    Add neovide feature pixiedust

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,3 +2,4 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.spelllang = {}
+vim.g.neovide_cursor_vfx_mode = "pixiedust"
