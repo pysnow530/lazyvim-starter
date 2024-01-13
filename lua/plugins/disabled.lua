683dc9d07d711da591647e06112583d1596608ce
pysnow530 <pysnow530@163.com>
2024-01-13 12:54:54 +0800

    Enable Noice

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,5 +1,5 @@
 return {
   { "rcarriga/nvim-notify", enabled = false },
   { "stevearc/dressing.nvim", enabled = false },
-  { "folke/noice.nvim", enabled = false },
+  -- { "folke/noice.nvim", enabled = false }, -- NOTE: 禁用掉后lua自动重启提示会打断工作流
 }
