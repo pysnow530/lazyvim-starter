299e7429e564ba665c72686a56cd2c2f100dd52f
pysnow530 <pysnow530@163.com>
2024-01-13 12:12:58 +0800

    Add live grep

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,5 +4,6 @@ return {
   "nvim-telescope/telescope.nvim",
   keys = {
     { "<C-p>", Util.telescope("files"), desc = "Find Files (root dir)" },
+    { "<leader>g", Util.telescope("live_grep"), desc = "Grep (root dir)" },
   },
 }
