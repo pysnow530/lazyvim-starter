429e41b3270134191e6accc5cd834cfe07decc08
pysnow530 <pysnow530@163.com>
2024-01-13 12:25:28 +0800

    Add exit shortcuts

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,3 +2,5 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.keymap.set("i", "jk", "<esc>")
+
+vim.keymap.set("n", "<leader>k", ":q<esc>")
