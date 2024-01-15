d0e00ad472a7540bd0614fbc38b7c5e8264d363d
pysnow530 <pysnow530@163.com>
2024-01-15 12:17:16 +0800

    Support git shortcuts, TODO shortcuts

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,12 +1,17 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local Util = require("lazyvim.util")
+
 vim.keymap.set("i", "jk", "<esc>")
 
 vim.keymap.set("n", "<leader>j", ":w<esc>")
 vim.keymap.set("n", "<leader>k", ":q<esc>")
 
-vim.keymap.set({ "n", "v" }, "<M-enter>", vim.lsp.buf.code_action)
+-- git version manager
+vim.keymap.set("n", "<leader>v", function()
+  Util.terminal({ "lazygit" }, { cwd = Util.root(), esc_esc = false, ctrl_hjkl = false })
+end)
 
 -- python
 vim.keymap.set("n", "<localleader>r", ":!python3 %<cr>")
---
+++ lua/plugins/todo-comments.lua
@@ -0,0 +1,6 @@
+return {
+  "folke/todo-comments.nvim",
+  keys = {
+    { "<leader>t", "<cmd>TodoTrouble keywords=TODO,FIX,FIXME<cr>", desc = "Todo/Fix/Fixme (Trouble)" },
+  },
+}
