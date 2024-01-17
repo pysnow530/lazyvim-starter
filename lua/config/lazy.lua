4b987b17a07576419cab66cbf840301afe5df148
pysnow530 <pysnow530@163.com>
2024-01-17 22:19:46 +0800

    Fix collision

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -18,6 +18,7 @@ require("lazy").setup({
     -- { import = "lazyvim.plugins.extras.lang.python-semshi" },
     -- { import = "lazyvim.plugins.extras.lang.yaml" },
     { import = "lazyvim.plugins.extras.lang.terraform" },
+    { import = "lazyvim.plugins.extras.lang.omnisharp" },
     -- import/override with your plugins
     { import = "plugins" },
   },
