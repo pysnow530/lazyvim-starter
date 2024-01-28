78022ed7ec11856c39c9c39274493be16d5d5558
pysnow530 <pysnow530@163.com>
2024-01-28 20:17:27 +0800

    修改备注

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,5 +1,5 @@
 return {
-  -- { "rcarriga/nvim-notify", enabled = false }, -- right top to show notify
-  -- { "stevearc/dressing.nvim", enabled = false }, -- show code action in center
+  -- { "rcarriga/nvim-notify", enabled = false }, -- NOTE: 在右上侧显示提示信息
+  -- { "stevearc/dressing.nvim", enabled = false }, -- NOTE: 在中间显示代码重构提示框
   -- { "folke/noice.nvim", enabled = false }, -- NOTE: 禁用掉后lua自动重启提示会打断工作流
 }
