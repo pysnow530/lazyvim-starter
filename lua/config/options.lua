47b12548c89393b842fa965bd5c3c5d75dddd892
pysnow530 <pysnow530@163.com>
2024-01-30 11:40:07 +0800

    输入时不隐藏鼠标指针

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -14,4 +14,4 @@ vim.g.neovide_cursor_vfx_mode = "pixiedust"
 -- vim.g.neovide_transparency = 0.98
 vim.g.neovide_floating_blur_amount_x = 2.0
 vim.g.neovide_floating_blur_amount_y = 2.0
-vim.g.neovide_hide_mouse_when_typing = true
+vim.g.neovide_hide_mouse_when_typing = false
